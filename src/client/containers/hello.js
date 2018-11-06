b1a8d47528fd1a980185cef5eeca6dab4872d776
52052100 <yl52052100@gmail.com>
2018-11-05 20:41:00 -0500

    motified hello.js

--- src/client/containers/hello.js
+++ src/client/containers/hello.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 
 class HelloWorld extends React.Component {
   render() {
-    return <p>Hello, world!</p>;
+    return <p>Hello, world!test</p>;
   }
 }
 
