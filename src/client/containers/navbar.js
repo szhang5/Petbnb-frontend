5ae7aac6b733a922fdab4cc462dfc84f3802709e
52052100 <yl52052100@gmail.com>
2018-11-18 13:06:16 -0500

    add sign in page

--- src/client/containers/navbar.js
+++ src/client/containers/navbar.js
@@ -5,18 +5,103 @@ class Navbar extends Component {
     return (
       <nav className="navbar navbar-light bg-light">
         <a className="navbar-brand" href=" ">
-          1
+          Home
         </a>
         <a className="navbar-brand" href="#">
-          2
+          Search Sitter
         </a>
         <a className="navbar-brand" href="#">
-          3
+          Our Service
         </a>
         <a className="navbar-brand" href="#">
-          4
+          Contact Us
         </a>
       </nav>
+      /*<ul class="nav nav-pills">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">
+            Home
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            Search Sitter
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            Our Service
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="#">
+            Contact Us
+          </a>
+        </li>
+      </ul>*/
+      /*<div>
+        <ul class="nav nav-tabs" id="myTab" role="tablist">
+          <li class="nav-item">
+            <a
+              class="nav-link active"
+              id="home-tab"
+              data-toggle="tab"
+              href="#home"
+              role="tab"
+              aria-controls="home"
+              aria-selected="true"
+            >
+              Home
+            </a>
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              id="profile-tab"
+              data-toggle="tab"
+              href="#profile"
+              role="tab"
+              aria-controls="profile"
+              aria-selected="false"
+            >
+              Profile
+            </a>
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              id="contact-tab"
+              data-toggle="tab"
+              href="#contact"
+              role="tab"
+              aria-controls="contact"
+              aria-selected="false"
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
+        <div class="tab-content" id="myTabContent">
+          <div
+            class="tab-pane fade show active"
+            id="home"
+            role="tabpanel"
+            aria-labelledby="home-tab"
+          />
+          <div
+            class="tab-pane fade"
+            id="profile"
+            role="tabpanel"
+            aria-labelledby="profile-tab"
+          />
+          <div
+            class="tab-pane fade"
+            id="contact"
+            role="tabpanel"
+            aria-labelledby="contact-tab"
+          />
+        </div>
+      </div>*/
     );
   }
 }
