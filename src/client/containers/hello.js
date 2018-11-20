311b54be85e6cefe032215a55080b64e3437b06a
wyfapril <yunfeiwang.april@gmail.com>
2018-11-19 19:52:59 -0500

    add sitter post

--- src/client/containers/hello.js
+++ src/client/containers/hello.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
+import SitterPost from "./sitter_post";
+import Grid from '@material-ui/core/Grid';
 
 class HelloWorld extends Component {
 
   render() {
     return (
-      <div>
-        Hello
-      </div>
+        <Grid container spacing={8}>
+            <SitterPost />
+            <SitterPost />
+            <SitterPost />
+            <SitterPost />
+        </Grid>
     );
   }
 }
