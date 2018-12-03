09ca7429bf83d79b1b2e2c9d55be32cdd7a1a514
shiyun zhang <shiyunzhang@shiyuns-MacBook-Pro.local>
2018-12-03 18:49:32 -0500

    add post store, add getPost() function

--- src/client/redux/reducers/index.js
+++ src/client/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import UserReducer from './user';
+import PostReducer from './post'
 // import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
   // form: formReducer,
   user: UserReducer,
+  post: PostReducer,
 });
 
 export default rootReducer;
