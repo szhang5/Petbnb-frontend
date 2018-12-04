20f94f38993601eafc2ede46d1f8a2e8b9e0927e
Shiyun Zhang <shiyun.zhang.zoey@gmail.com>
2018-12-03 20:16:11 -0500

    add Post.js returns

--- src/client/redux/reducers/post.js
+++ src/client/redux/reducers/post.js
@@ -2,8 +2,15 @@ import * as ACTIONS from "../actions/index";
 
 const initialState = {
   posts : [],
-
+  'avai_end_date': '',
+	'avai_start_date': '',
+	'description': '',
+	'hour_rate': '',
+	'pet_type': '',
+	'pets_num': '',
+	'postdate': '',
 }
+
 
 export default function(state = initialState, action) {
   switch(action.type) {
@@ -11,6 +18,23 @@ export default function(state = initialState, action) {
 	    return {
         'posts': action.payload.data.post,
 	    };
+	  case ACTIONS.CREATE_POST_ACTION:
+	    return initialState;
+	  case ACTIONS.SEARCH_POST_ACTION:
+	  	return {
+	  		'posts': action.payload.data.post,
+	  	}
+	  case ACTIONS.GET_USER_POST_ACTION:
+	  	// console.log('action: ', action);
+	  	return {
+	  		'avai_end_date': action.payload.data.post.avai_end_date,
+				'avai_start_date': action.payload.data.post.avai_start_date,
+				'description': action.payload.data.post.description,
+				'hour_rate': action.payload.data.post.hour_rate,
+				'pet_type': action.payload.data.post.pet_type,
+				'pets_num': action.payload.data.post.pets_num,
+				'postdate': action.payload.data.post.postdate,
+	  	}
     default:
       return state;
   }
