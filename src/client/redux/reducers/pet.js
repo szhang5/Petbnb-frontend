6fc72a5f1e39f3d0249bde1d8c19651320ac1ca6
Shiyun Zhang <shiyun.zhang.zoey@gmail.com>
2019-02-19 23:48:51 -0500

    modified pet page

--- src/client/redux/reducers/pet.js
+++ src/client/redux/reducers/pet.js
@@ -1,47 +1,49 @@
 import * as ACTIONS from "../actions/index";
 
 const initialState = {
- //   name: "",
-    id:"",
-    uid:"",
-    birth:"",
-    petname:"",
+    id: 0,
+    uid: 0,
+    birth: "",
+    petname: "",
     type: "",
     breed: "",
     furcolor: "",
     weight: "",
-    image:""
+    image: "",
 };
 
 export default function(state = initialState, action) {
-   // console.log("pet20"+action);
   switch (action.type) {
-    case ACTIONS.EDIT_PET_PROFILE_ACTION:
-  
+    case ACTIONS.GET_PET_INFO: 
       return {
-        name: action.payload.data.pet.name,
-         id:action.payload.data.pet.id,
-         uid:action.payload.data.pet.uid,
-         birth:action.payload.data.pet.birth,
-         petname:action.payload.data.pet.petname,
-         type: action.payload.data.pet.type,
-         breed: action.payload.data.pet.breed,
-         furcolor: action.payload.data.pet.furcolor,
-         weight: action.payload.data.pet.weight,
-         image:action.payload.data.pet.image,
+        pets: action.payload.data.pet,
+      }
+    case ACTIONS.EDIT_PET_PROFILE_ACTION:
+      return {
+        id:action.payload.data.id,
+        uid:action.payload.data.uid,
+        birth:action.payload.data.birth,
+        petname:action.payload.data.petname,
+        type: action.payload.data.type,
+        breed: action.payload.data.breed,
+        furcolor: action.payload.data.furcolor,
+        weight: action.payload.data.weight,
+        image:action.payload.data.image,
       };
       case ACTIONS.CREATE_PET_PROFILE_ACTION:
       return{
         ...state,
-          uid:action.payload.data.pet.uid,
-         
+          uid:action.payload.data.uid,
       }
       case ACTIONS.UPDATE_PET_INFO:
-      console.log("pet20"+action.field);
       return {
-
           ...state,
           [action.field]:action.value,
+      }
+      case ACTIONS.UPDATE_PET_INFO:
+      return {
+        ...state,
+        image: action.payload.data.imageUrl,
       }
     default:
       return state;
