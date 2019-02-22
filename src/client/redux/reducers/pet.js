2ef2ec3d64e68ca2bd2d1dcd30af9004beea0f07
Shiyun Zhang <shiyun.zhang.zoey@gmail.com>
2019-02-21 21:38:12 -0500

    added CreatePetProfile.js

--- src/client/redux/reducers/pet.js
+++ src/client/redux/reducers/pet.js
@@ -1,8 +1,10 @@
 import * as ACTIONS from "../actions/index";
+import moment from "moment";
+
 
 const initialState = {
     petid: 1,
-    uid: 0,
+    uid: null,
     birth: "",
     petname: "",
     type: "",
@@ -18,10 +20,10 @@ export default function(state = initialState, action) {
     case ACTIONS.GET_PET_INFO_BY_ID: 
     console.log(action);
     return {
-      petid:action.payload.data.petid,
+      petid: action.payload.data.petid,
       uid: action.payload.data.uid,
-      birth:action.payload.data.birth,
-      petname:action.payload.data.petname,
+      birth: moment(action.payload.data.birth).format('YYYY-MM-DD'),
+      petname: action.payload.data.petname,
       type: action.payload.data.type,
       breed: action.payload.data.breed,
       furcolor: action.payload.data.furcolor,
@@ -36,9 +38,9 @@ export default function(state = initialState, action) {
     //console.log("pet20");
       return {
         ...state,
-        petid:action.payload.data.petid,
-        birth:action.payload.data.birth,
-        petname:action.payload.data.petname,
+        petid: action.payload.data.petid,
+        birth: moment(action.payload.data.birth).format('YYYY-MM-DD'),
+        petname: action.payload.data.petname,
         type: action.payload.data.type,
         breed: action.payload.data.breed,
         furcolor: action.payload.data.furcolor,
@@ -47,8 +49,13 @@ export default function(state = initialState, action) {
       case ACTIONS.CREATE_PET_PROFILE_ACTION:
       return{
         ...state,
-          uid:action.payload.data.uid,
-         
+          uid: action.payload.data.uid,
+          birth: moment(action.payload.data.birth).format('YYYY-MM-DD'),
+          petname: action.payload.data.petname,
+          type: action.payload.data.type,
+          breed: action.payload.data.breed,
+          furcolor: action.payload.data.furcolor,
+          weight: action.payload.data.weight,
       };
       case ACTIONS.UPDATE_PET_INFO:
       return {
