fc23a9db0e877e518416ce8fb5ab6bd23ee4da1b
shiyun zhang <shiyunzhang@shiyuns-MacBook-Pro.local>
2019-03-23 23:30:09 -0400

    connect transaction page to database

--- src/client/containers/transactionCard.js
+++ src/client/containers/transactionCard.js
@@ -1,12 +1,16 @@
 import React, { Component }  from 'react';
 import PropTypes from 'prop-types';
+import { map } from "lodash";
 import { withStyles } from '@material-ui/core/styles';
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
 import Avatar from '@material-ui/core/Avatar';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Chip from '@material-ui/core/Chip';
 import DoneIcon from '@material-ui/icons/Done';
 import Grid from '@material-ui/core/Grid';
+import { getUserTransaction, updateTransactionStatus } from "../redux/actions";
 import Typography from '@material-ui/core/Typography';
 
 const styles = {
@@ -18,118 +22,160 @@ const styles = {
     }
 };
 
-function handleAccept() {
-    alert('You accepted the request.'); // eslint-disable-line no-alert
-}
+// function handleAccept() {
+//     alert('You accepted the request.'); // eslint-disable-line no-alert
+// }
 
-function handleDecline() {
-    alert('You declined the request.'); // eslint-disable-line no-alert
-}
+
+// function handleDecline() {
+//     alert('You declined the request.'); // eslint-disable-line no-alert
+// }
 
 class TransactionCard extends Component {
 
-    render() {
-        const { classes } = this.props;
+  handleStatusChange = (transacid, status) =>{
+    this.props.updateTransactionStatus(transacid, status);
+  }
 
-        return (
-            <div>
-                <h1>Reservation</h1>
-                <Card className={classes.card}>
-                    <CardContent>
-                        <Grid container justify="center" alignItems="center">
-                            <Avatar alt="pet pic" src="https://cdn.petcarerx.com/testimonials/testimony-2.png" className={classes.avatar} />
-                        </Grid>
-                        <Typography>
-                            Owner: Eric Johnson
-                        </Typography>
-                        <Typography>
-                            Date: 03/01/2019-03/20/2019
-                        </Typography>
-                        <Typography>
-                            Amount: $100
-                        </Typography>
-                        <Grid container>
-                            <Grid item xs>
-                                <Chip
-                                    label="Accept"
-                                    clickable
-                                    className={classes.chip}
-                                    color="primary"
-                                    onDelete={handleAccept}
-                                    deleteIcon={<DoneIcon />}
-                                    variant="outlined"
-                                />
-                            </Grid>
-                            <Grid item xs>
-                                <Chip
-                                    label="Decline"
-                                    onDelete={handleDecline}
-                                    className={classes.chip}
-                                    color="secondary"
-                                    variant="outlined"
-                                />
-                            </Grid>
-                        </Grid>
-                    </CardContent>
-                </Card>
-                <Card className={classes.card}>
-                    <CardContent>
-                        <Grid container justify="center" alignItems="center">
-                            <Avatar alt="pet pic" src="https://cdn.petcarerx.com/testimonials/testimony-2.png" className={classes.avatar} />
-                            <Avatar alt="pet pic" src="https://cdn.petcarerx.com/testimonials/testimony-2.png" className={classes.avatar} />
-                            <Avatar alt="pet pic" src="https://cdn.petcarerx.com/testimonials/testimony-2.png" className={classes.avatar} />
-                        </Grid>
-                        <Typography>
-                            Owner: Emily Williams
-                        </Typography>
-                        <Typography>
-                            Date: 04/01/2019-04/10/2019
-                        </Typography>
-                        <Typography>
-                            Amount: $200
-                        </Typography>
-                        <Grid container>
-                            <Grid item xs>
-                                <Chip
-                                    label="Accepted"
-                                    className={classes.chip}
-                                />
-                            </Grid>
-                        </Grid>
-                    </CardContent>
-                </Card>
-                <Card className={classes.card}>
-                    <CardContent>
-                        <Grid container justify="center" alignItems="center">
-                            <Avatar alt="pet pic" src="https://cdn.petcarerx.com/testimonials/testimony-2.png" className={classes.avatar} />
-                            <Avatar alt="pet pic" src="https://cdn.petcarerx.com/testimonials/testimony-2.png" className={classes.avatar} />
-                        </Grid>
-                        <Typography>
-                            Owner: Tom Baker
-                        </Typography>
-                        <Typography>
-                            Date: 02/01/2019-02/28/2019
-                        </Typography>
-                        <Typography>
-                            Amount: $300
-                        </Typography>
-                        <Grid container>
-                            <Grid item xs>
-                                <Chip
-                                    label="Payment Recieved"
-                                    className={classes.chip}
-                                    color="default"
-                                />
-                            </Grid>
-                        </Grid>
-                    </CardContent>
-                </Card>
-            </div>
-        );
-    }
+  componentWillMount(){
+      this.props.getUserTransaction(this.props.uid);
+  }
+
+  render() {
+    const { 
+        classes,
+        transactions,
+        user_type,
+    } = this.props;
+
+    return (
+       <div>
+        <h1>Reservation</h1>
+        {map(transactions, (transaction, key) => { 
+          let transacinfo = transaction.transacinfo;
+          let owner = transaction.owner;
+          let sitter = transaction.sitter;
+          let pets = transaction.pets;
+          return (
+            <Card className={classes.card} key={transacinfo.transacid}>
+              <CardContent>
+              <Grid container justify="center" alignItems="center">
+              {map(pets, (pet, key) => {
+                return(
+                  <Avatar alt="pet pic" src={pet.image} className={classes.avatar}  key = {pet.petid}/>
+                )
+              })}
+               </Grid>
+                  <Typography>
+                      {user_type == 0? `Pet Owner: ${owner.firstname} ${owner.lastname}` : `Pet Sitter: ${sitter.firstname} ${sitter.lastname}`}
+                  </Typography>
+                  <Typography>
+                      Date: 03/01/2019-03/20/2019
+                  </Typography>
+                  <Typography>
+                      Amount: $100
+                  </Typography>
+                  {transacinfo.status == 0 && <Grid container>
+                      <Grid item xs>
+                          <Chip
+                              label="Accept"
+                              clickable
+                              className={classes.chip}
+                              color="primary"
+                              onClick={() => this.handleStatusChange(transacinfo.transacid, 1)}
+                              deleteIcon={<DoneIcon />}
+                              variant="outlined"
+                          />
+                      </Grid>
+                      <Grid item xs>
+                          <Chip
+                              label="Decline"
+                              onClick={() => this.handleStatusChange(transacinfo.transacid, 3)}
+                              className={classes.chip}
+                              color="secondary"
+                              variant="outlined"
+                          />
+                      </Grid>
+                  </Grid> }
+
+                  {user_type == 0 && transacinfo.status == 1 && <Grid container>
+                      <Grid item xs>
+                          <Chip
+                              label="Accepted"
+                              className={classes.chip}
+                          />
+                      </Grid>
+                  </Grid>}
+
+                  {user_type == 1 && transacinfo.status == 1 && <Grid container>
+                      <Grid item xs>
+                          <Chip
+                              label="Pay"
+                              onClick={() => this.handleStatusChange(transacinfo.transacid, 2)}
+                              className={classes.chip}
+                              color="Primary"
+                              variant="outlined"
+                          />
+                      </Grid>
+                  </Grid>}
+
+                  {transacinfo.status == 2 && <Grid container>
+                      <Grid item xs>
+                          <Chip
+                              label="Payment Received"
+                              color="#00ff00"
+                              className={classes.chip}
+                          />
+                      </Grid>
+                  </Grid>}
+                  {transacinfo.status == 3 && <Grid container>
+                      <Grid item xs>
+                          <Chip
+                              label="Declined"
+                              className={classes.chip}
+                          />
+                      </Grid>
+                  </Grid>}
+              </CardContent>
+          </Card>
+         );
+      })}
+      </div>
+    );
+  }
 }
 
 TransactionCard.propTypes = {
     classes: PropTypes.object.isRequired,
+    uid: PropTypes.oneOfType([PropTypes.string,PropTypes.number]),
+    transactions: PropTypes.array,
+    user_type: PropTypes.oneOfType([PropTypes.string,PropTypes.number]),
 };
-export default withStyles(styles)(TransactionCard);
+
+
+TransactionCard.defaultProps = {
+  uid : 0,
+  transactions: [],
+  user_type: 0,
+};
+
+function mapStateToProps({ user, transaction }) {
+  return {
+    uid : user.uid,
+    user_type: user.user_type,
+    transactions : transaction.transactions,
+
+  };
+}
+
+export default withRouter(
+  withStyles(styles)(
+    connect(
+      mapStateToProps,
+      { getUserTransaction, updateTransactionStatus }
+    )(TransactionCard)
+  )
+);
+
+
+
